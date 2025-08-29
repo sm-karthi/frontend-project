0a790683221b4537ec9fe0eaf2dd37c02011439a
sm-karthi <karthikeyan24022007@gmail.com>
2025-08-29 12:08:10 +0530

    Update index.ts

--- src/utils/api/index.ts
+++ src/utils/api/index.ts
@@ -1,14 +1,14 @@
 import { CommentData } from "@/types";
 import axios from "axios";
 
-const baseUrl = process.env.BASE_URL;
-console.log("Base URL", baseUrl);
+// const baseUrl = process.env.BASE_URL;
+// console.log("Base URL", baseUrl);
 
 
 export function postComment(data: CommentData) {
-    return axios.post(`${baseUrl}/api`, data);
+    return axios.post(`https://rv-swart.vercel.app/api`, data);
 }
 
 export function getComments(blogId: string) {
-    return axios.get(`${baseUrl}/api?blogId=${blogId}`);
+    return axios.get(`https://rv-swart.vercel.app/api?blogId=${blogId}`);
 }
